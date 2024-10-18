392612c4f51dd86ade8425ff057a799e8578294e
Luka Ferlež <luka.ferlez@bozic.io>
2024-10-18 15:45:28 +0200

    ✨ feat: Bicep management powershell module

---
+++ src/Simpleverse-Bicep.psd1
@@ -0,0 +1,131 @@
+# Module manifest for module 'Simpleverse-Bicep'
+
+@{
+
+	# Script module or binary module file associated with this manifest.
+	RootModule = 'Simpleverse-Bicep.psm1'
+	
+	# Version number of this module.
+	ModuleVersion     = '2024.10.17.7'
+	
+	# Supported PSEditions
+	# CompatiblePSEditions = @()
+	
+	# ID used to uniquely identify this module
+	GUID              = '48b779c6-c4d0-42a9-a51f-004f76bd1114'
+	
+	# Author of this module
+	Author            = 'luka@ferlez.hr'
+	
+	# Company or vendor of this module
+	CompanyName       = 'Simpleverse'
+	
+	# Copyright statement for this module
+	Copyright         = '(c) luka@ferlez. All rights reserved.'
+	
+	# Description of the functionality provided by this module
+	Description = 'Provides functions to manage Bicep modules.
+		* Get-BicepImports - Returns list of imports/modules used in Bicep files.
+		* Get-BicepImpactedModules - Returns list of impacted modules impacted by changes between two commits. It will include modules changes following the import hierarchy provuiding a full list of impacted modules.
+		* Get-BicepModulesToPublish - Returns list of Bicep modules to publish based on the changes between two commits or all modules based on parameters. It will include modules changes following the import hierarchy provuiding a full list of impacted modules.
+		* Publish-BicepModules - Publishes Bicep modules to Azure ACR.
+		* Update-BicepModulesVersion - Updates the version of the imports & modules from custom repositories to the latest version available in the registry. Currently supports only Azure ACR.
+	'
+	
+	# Minimum version of the PowerShell engine required by this module
+	PowerShellVersion = '7.0'
+	
+	# Name of the PowerShell host required by this module
+	# PowerShellHostName = ''
+	
+	# Minimum version of the PowerShell host required by this module
+	# PowerShellHostVersion = ''
+	
+	# Minimum version of Microsoft .NET Framework required by this module. This prerequisite is valid for the PowerShell Desktop edition only.
+	# DotNetFrameworkVersion = ''
+	
+	# Minimum version of the common language runtime (CLR) required by this module. This prerequisite is valid for the PowerShell Desktop edition only.
+	# CLRVersion = ''
+	
+	# Processor architecture (None, X86, Amd64) required by this module
+	# ProcessorArchitecture = ''
+	
+	# Modules that must be imported into the global environment prior to importing this module
+	# RequiredModules = @()
+	
+	# Assemblies that must be loaded prior to importing this module
+	# RequiredAssemblies = @()
+	
+	# Script files (.ps1) that are run in the caller's environment prior to importing this module.
+	# ScriptsToProcess = @()
+	
+	# Type files (.ps1xml) to be loaded when importing this module
+	# TypesToProcess = @()
+	
+	# Format files (.ps1xml) to be loaded when importing this module
+	# FormatsToProcess = @()
+	
+	# Modules to import as nested modules of the module specified in RootModule/ModuleToProcess
+	# NestedModules = @()
+	
+	# Functions to export from this module, for best performance, do not use wildcards and do not delete the entry, use an empty array if there are no functions to export.
+	FunctionsToExport = @("Update-BicepModulesVersion", "Publish-BicepModules", "Get-BicepModulesToPublish", "Get-BicepImports", "Get-BicepImpactedModules")
+	
+	# Cmdlets to export from this module, for best performance, do not use wildcards and do not delete the entry, use an empty array if there are no cmdlets to export.
+	CmdletsToExport   = @()
+	
+	# Variables to export from this module
+	VariablesToExport = '*'
+	
+	# Aliases to export from this module, for best performance, do not use wildcards and do not delete the entry, use an empty array if there are no aliases to export.
+	AliasesToExport   = @()
+	
+	# DSC resources to export from this module
+	# DscResourcesToExport = @()
+	
+	# List of all modules packaged with this module
+	# ModuleList = @()
+	
+	# List of all files packaged with this module
+	# FileList = @()
+	
+	# Private data to pass to the module specified in RootModule/ModuleToProcess. This may also contain a PSData hashtable with additional module metadata used by PowerShell.
+	PrivateData       = @{
+	
+		PSData = @{
+	
+			# Tags applied to this module. These help with module discovery in online galleries.
+			# Tags = @()
+	
+			# A URL to the license for this module.
+			# LicenseUri = ''
+	
+			# A URL to the main website for this project.
+			# ProjectUri = ''
+	
+			# A URL to an icon representing this module.
+			# IconUri = ''
+	
+			# ReleaseNotes of this module
+			# ReleaseNotes = ''
+	
+			# Prerelease string of this module
+			# Prerelease = ''
+	
+			# Flag to indicate whether the module requires explicit user acceptance for install/update/save
+			# RequireLicenseAcceptance = $false
+	
+			# External dependent modules of this module
+			# ExternalModuleDependencies = @()
+	
+		} # End of PSData hashtable
+	
+	} # End of PrivateData hashtable
+	
+	# HelpInfo URI of this module
+	# HelpInfoURI = ''
+	
+	# Default prefix for commands exported from this module. Override the default prefix using Import-Module -Prefix.
+	# DefaultCommandPrefix = ''
+	
+}
---
+++ src/Simpleverse-Bicep.psm1
@@ -0,0 +1,365 @@
+class BicepImport {
+	[ValidateNotNullOrEmpty()][string]$Alias
+	[ValidateNotNullOrEmpty()][string]$Name
+	[string]$Version
+	[string]$RegistryUrl
+	[string]$LatestVersion
+	[array]$FilePaths
+}
+
+function Get-BicepImports([string] $pathSpec) {
+	Write-Host "Get-BicepImports: $pathSpec"
+	$moduleReferences = Get-ChildItem -recurse -Path $pathSpec | Select-String -pattern "\bmodule\b", "\bimport\b" | Select-Object
+
+	$modules = @()
+	for(($index = 0); $index -lt $moduleReferences.Count; $index++) {
+		$moduleReference = $moduleReferences[$index]
+
+		Write-DebugIndexed $index "Reference $($moduleReference)"
+		Write-DebugIndexed $index "Line: '$($moduleReference.Line)'"
+
+		$beginIndex = $moduleReference.Line.IndexOf("'")+1
+		$endIndex = $moduleReference.Line.IndexOf("'", $beginIndex)		
+		Write-DebugIndexed $index "Begin: $($beginIndex) - End: $($endIndex)"
+
+		$module = $moduleReference.Line.SubString($beginIndex, $endIndex - $beginIndex)
+		Write-DebugIndexed $index "Module: '$($module)'"
+
+		$alias = ''
+		$name = ''
+		$version = ''
+
+		if ($module.Contains(':')) {
+			$moduleParts = $module.Split(':')
+
+			$alias = $moduleParts[0]
+			$name = $moduleParts[1]
+			$version = $moduleParts[2]
+		} elseif ($module.Contains('@')) {
+		} elseif ($module.Contains('.bicep')) {
+			$fileDir = Split-Path -Path $moduleReference.Path -Parent
+			Write-DebugIndexed $index "FileDir: $($fileDir)"
+			$moduleName = Resolve-Path "$($fileDir)/$($module)" -Relative
+			Write-DebugIndexed $index "ModuleName: $($moduleName)"
+
+			$alias = '.'
+			$name = $moduleName
+			$version = ''
+		}
+
+		$existingModule = $modules | Where-Object { $_.Alias -eq $alias -And $_.Name -eq $name}
+		if ($null -eq $existingModule) {
+			$modules += [BicepImport]@{
+				Alias = $alias
+				Name = $name
+				Version = $version
+				FilePaths = @($moduleReference.Path)
+			}
+		} else  {
+			if ($existingModule.FilePaths -notcontains $moduleReference.Path) {
+				$existingModule.FilePaths += $moduleReference.Path
+			}
+		}
+		Write-Debug "-------------- END REFERENCE $($index) --------------"
+	}
+
+	Write-Host "Get-BicepImports: Found $($modules.Count) imports." -ForegroundColor Green
+	$modules | Select-Object Alias, Name, Version, FilePaths | Format-Table | Out-String | Write-Host
+	return $modules
+}
+
+Export-ModuleMember Get-BicepImports
+
+class BicepModule {
+	[string] $FilePath
+	[string] $Name
+
+	BicepModule() {}
+
+	BicepModule([string] $fullPath) {
+		$this.FilePath = Resolve-Path -Relative $fullPath
+		$this.Name = $this.FilePath -replace "^./" -replace '^.\\' -replace '\..*'
+	}
+
+	BicepModule([string] $filePath, [string] $name) {
+		FilePath = $filePath
+		Name = $name
+	}
+}
+
+function Get-BicepImpactedModules {
+	Param(
+		[Parameter(Mandatory=$true,	Position=0, HelpMessage="PathSpec to grep Bicep modules to publish.")]
+		[string] $PathSpec,
+		[Parameter(Mandatory=$true,	Position=1, HelpMessage="Commit range to check for changes.")]
+		[string] $CommitRange,
+		[Parameter(Mandatory=$false, HelpMessage="Exclude direct changes to files in pathSpec from being published.")]
+		[Alias("ed")]
+		[switch] $ExcludeDirectChanges
+	)
+
+	Write-Host "Get-ImpactedModules: $PathSpec - $CommitRange"	
+	
+	$changedFiles = git diff-tree --no-commit-id --name-only --diff-filter=d -r $CommitRange $PathSpec
+	Write-Debug "Found $($changedFiles.Count) changed files."
+	$changedFiles | Format-Table | Out-String | Write-Debug
+
+	$changedModules = @()
+	foreach ($file in $changedFiles) {
+		$changedModules += [BicepModule]::new($file)
+	}
+
+	Write-Host "Get-ImpactedModules: Found $($changedModules.Count) changed modules." -ForegroundColor Green
+	$changedModules | Select-Object Name, FilePath | Format-Table | Out-String | Write-Host
+
+	$bicepImports = Get-BicepImports $PathSpec
+
+	function Get-ImpactedModules2 {
+		Param(
+			[BicepModule] $changedModule,
+			$imports
+		)
+		Write-Debug "Get-ImpactedModules: $($changedModule.FilePath) - Resolving impacted modules"
+
+		$impactedModules = @($changedModule)
+
+		$import = $imports | Where-Object { $_.Name -eq $changedModule.FilePath } | Select-Object -First 1
+		if ($null -ne $import) {
+			Write-Debug "Get-ImpactedModules: $($changedModule.FilePath) - Discovered dependecies"
+
+			foreach($filePath in $import.FilePaths) {
+				Write-Debug "Get-ImpactedModules: $($changedModule.FilePath) - Resolving impacted modules for $($filePath)"
+
+				$module = [BicepModule]::new($filePath)
+				$impactedModules += Get-ImpactedModules2 $module $imports
+			}
+		}
+
+		return $impactedModules
+	}
+
+	$impactedModules = @()
+	foreach($changedModule in $changedModules) {
+		$impactedModules += Get-ImpactedModules2 $changedModule $bicepImports
+	}
+
+	$impactedModules  = $impactedModules | Group-Object -Property 'Name', 'FilePath' | %{ $_.Group | Select-Object 'Name', 'FilePath' -First 1 } | Sort-Object 'Name'
+	if ($ExcludeDirectChanges) {
+		$reducedModules = @()
+		foreach ($module in $impactedModules) {
+			$existingModule = $changedModules | Where-Object { $_.Name -eq $module.Name }
+			if ($null -eq $existingModule) {
+				$reducedModules += $module
+			}
+		}
+
+		$impactedModules = $reducedModules
+	}
+
+	Write-Host "Get-ImpactedModules: Found $($impactedModules.Count) impacted modules." -ForegroundColor Green
+	$impactedModules | Select-Object Name, FilePath | Format-Table | Out-String | Write-Host
+
+	return $impactedModules
+}
+
+Export-ModuleMember Get-BicepImpactedModules
+
+function Get-BicepModulesToPublish {
+	Param(
+		[Parameter(Mandatory=$true,	Position=0, HelpMessage="PathSpec to grep Bicep modules to publish.")]
+		[string] $PathSpec,
+		[Parameter(Mandatory=$true,	Position=1, HelpMessage="Commit range to check for changes.")]
+		[string] $CommitRange,
+		[Parameter(Mandatory=$false, HelpMessage="Include only changed Bicep modules.")]
+		[Alias("c")]
+		[switch] $IncludeNotChanged,
+		[Parameter(Mandatory=$false, HelpMessage="Exclude direct changes to files in pathSpec from being published.")]
+		[Alias("ed")]
+		[switch] $ExcludeDirectChanges
+	)
+
+	$modulesToPublish = @()
+	if ($IncludeNotChanged) {
+		$files = Get-ChildItem -Recurse -Path $PathSpec
+		$modulesToPublish = @()
+		foreach ($file in $files) {
+			$modulesToPublish += [BicepModule]::new($file)
+		}
+	} else {
+		$modulesToPublish = Get-BicepImpactedModules $PathSpec $CommitRange -ExcludeDirectChanges:$ExcludeDirectChanges
+	}
+
+	Write-Host "Get-ModulesToPublish: Found $($modulesToPublish.Count) files to publish." -ForegroundColor Green
+	$modulesToPublish | Format-Table | Out-String | Write-Host
+	
+	return $modulesToPublish
+}
+
+Export-ModuleMember Get-BicepModulesToPublish
+
+<#
+.SYNOPSIS
+
+Publishes changed Bicep modules to the Azure Container Registry (ACR).
+
+.DESCRIPTION
+
+Extracts changed files based on a pathspec and commit range.
+Checks for usage in imports and module declarations on the same pathspace.
+Publishes changed modules and dependants with a new version to the registry.
+Current support is limited to Azure Container Registry (ACR).
+
+.INPUTS
+
+None. You cannot pipe objects to Add-Extension.
+
+.OUTPUTS
+
+None.
+
+.EXAMPLE
+
+PS> Publish-BicepModules '*.bicep' 'd41eeb1c7c0a6a5e3f11efc175aa36b8eaae4af5..0ee2650f101237af9ad923ad2264d37b983d8bab' someacr '2024.10.17.1'
+
+.LINK
+
+http://www.fabrikam.com/extension.html
+
+#>
+function Publish-BicepModules {
+	Param(
+		[Parameter(Mandatory = $true, Position = 0, HelpMessage = "PathSpec to grep Bicep modules to publish.")]
+		[string] $PathSpec,
+		[Parameter(Mandatory = $true, Position = 1, HelpMessage = "Commit range to check for changes.")]
+		[string] $CommitRange,
+		[Parameter(Mandatory = $true, Position = 2, HelpMessage = "Registry name of Azure container registry to which to publish.")]
+		[string] $RegistryName,		
+		[Parameter(Mandatory = $false, Position = 3, HelpMessage = "Version to be tagged to published modules.")]
+		[string] $Version,
+		[Parameter(Mandatory=$false, HelpMessage="Include only changed Bicep modules.")]
+		[Alias("c")]
+		[switch] $IncludeNotChanged,
+		[Parameter(Mandatory = $false, HelpMessage = "Exclude direct changes to files in pathSpec from being published.")]
+		[Alias("ed")]
+		[switch] $ExcludeDirectChanges
+	)
+
+	$modulesToPublish = Get-BicepModulesToPublish $PathSpec $CommitRange -IncludeNotChanged:$IncludeNotChanged -ExcludeDirectChanges:$ExcludeDirectChanges
+
+	foreach ($module in $modulesToPublish) {
+		Write-Host "Publishing module $($module.Name) with version $($Version) to registry $($RegistryName)"
+		az bicep publish --file $module.FilePath --target "br:$($RegistryName).azurecr.io/$($module.Name):$($Version)" --only-show-errors
+	}
+}
+
+Export-ModuleMember Publish-BicepModules
+
+class FileToUpdate {
+	[string]$Path
+	[array]$Modules
+}
+
+<#
+.SYNOPSIS
+
+Updates the versions of the imports & modules from custom repositories to the latest version available in the registry.
+
+.DESCRIPTION
+
+Extracts from all files mathcing the pathspec, imports & module declarations that are using the custom repository syntax alias:modulename:version.
+Checks a newer version in the registry and updates the version in the files to the latest version available in the registry.
+Current support is limited to Azure Container Registry (ACR).
+
+.INPUTS
+
+None. You cannot pipe objects to Add-Extension.
+
+.OUTPUTS
+
+None.
+
+.EXAMPLE
+
+PS> Update-BicepModulesVersion '*.bicep'
+
+.LINK
+
+http://www.fabrikam.com/extension.html
+
+#>
+function Update-BicepModulesVersion {
+	Param(
+		[Parameter(Mandatory = $true, Position = 0, HelpMessage = "PathSpec to grep Bicep modules to update.")]
+		[string] $PathSpec,
+		[Parameter(Mandatory = $false, HelpMessage = "Path to bicepconfig.json with defined registries.")]
+		[Alias("b")]
+		[string] $BicepConfigPath = 'bicepconfig.json'
+	)
+
+	$modules = Get-BicepImports $PathSpec | Where-Object { $_.Alias -ne '.' }
+
+	Write-Host "Found $($modules.Count) modules." -ForegroundColor Green
+	$modules | Select-Object Alias, Name, Version | Format-Table | Out-String | Write-Host
+
+	Write-Host "Gathering latest versions from registry source."
+	$bicepConfig = Get-Content $BicepConfigPath | ConvertFrom-Json -AsHashtable
+	foreach ($module in $modules) {
+		$aliasSplit = $module.Alias.Split("/")
+		$module.registryUrl = $bicepConfig['moduleAliases'][$aliasSplit[0]][$aliasSplit[1]]['registry']
+		Write-Host "Checking $($module.Alias) from registry $($module.registryUrl) for $($module.Name)"
+		$module.LatestVersion = az acr repository show-tags --name $module.RegistryUrl.Replace('.azurecr.io', '') --repository $module.Name --top 1 --orderby time_desc | ConvertFrom-Json
+	}
+
+	$modulesForUpdate = $modules | Where-Object { $_.Version -ne $_.LatestVersion }
+
+	if ($modulesForUpdate.Count -eq 0) {
+		Write-Host "All modules are up to date." -ForegroundColor Green
+		return
+	}
+
+	Write-Host "Modules to update."
+	$modules | Where-Object { $_.Version -ne $_.LatestVersion } | Select-Object Alias, Name, Version, LatestVersion | Format-Table
+
+	$update = Read-Host "Update? (Y/N)"
+	if ($update -ne 'Y' -or $update -ne 'y') {
+		return
+	}
+
+	$filesToUpdate = @()
+	foreach ($module in $modules | Where-Object { $_.Version -ne $_.LatestVersion }) {
+		foreach ($filePath in $module.FilePaths) {
+			$existingFilePath = $filesToUpdate | Where-Object { $_.Path -eq $filePath }
+			if ($null -eq $existingFilePath) {
+				$filesToUpdate += [FileToUpdate]@{
+					Path = $filePath
+					Modules = @($module)
+				}
+			} else {
+				$existingFilePath.Modules += $module
+			}
+		}
+	}
+
+	$filesToUpdate | Format-Table | Out-String | Write-Host
+
+	foreach ($fileToUpdate in $filesToUpdate) {
+		$content = Get-Content $fileToUpdate.Path
+		foreach ($module in $fileToUpdate.Modules) {
+			$content = $content -replace "$($module.Alias):$($module.Name):$($module.Version)", "$($module.Alias):$($module.Name):$($module.LatestVersion)"
+		}
+		Set-Content -Path $fileToUpdate.Path -Value $content
+	}
+
+	Write-Host "Updated $($filesToUpdate.Count) files." -ForegroundColor Green
+}
+
+Export-ModuleMember Update-BicepModulesVersion
+
+function Write-DebugIndexed {
+	Param(
+		[int] $index,
+		[string] $message
+	)
+	Write-Debug "Reference #$($index) - $($message)"
+}
+
